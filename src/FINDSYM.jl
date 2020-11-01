9bb90d340316a5fd4457a96bf1c0b3fa079e0eb4
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-01 08:27:19 +0800

    master

--- src/FINDSYM.jl
+++ src/FINDSYM.jl
@@ -83,12 +83,9 @@ function improve_cif(title::AbstractString, old_cif_fn::AbstractString)
         "cell_volume",
         "cell_formula_units_Z"
     ]
-    lines_to_keep = String[strip(extract_kw(old_cif_fn, kw)) for kw in keep_info_kw]
+    lines_to_keep = String[strip(first(extract_kw(old_cif_fn, kw))) for kw in keep_info_kw]
     gen_cif_lines = generate_cif(title, findsym_cifinput(old_cif_fn))
     pos = findfirst(x->occursin("cell_volume",x), gen_cif_lines)
-    @info gen_cif_lines[pos-1]
-    @info gen_cif_lines[pos]
-    @info gen_cif_lines[pos+1]
     if pos === nothing
         @warn "improve_cif($title, \n$old_cif_fn) : \nOld lines not kept because kw _cell_volume not found."
         return gen_cif_lines
