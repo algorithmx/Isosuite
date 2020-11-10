f77d8c411cc1f7285a76dcdb806c511b25180f6b
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-10 11:29:48 +0800

    master

--- scripts/example.jl
+++ scripts/example.jl
@@ -6,7 +6,7 @@ fn = ARGS[1]
 println(ispath(fn))
 println(isfile(fn))
 
-if !isfile(fn) && ispath(fn)
+if !isfile(fn) && isdir(fn)
     files = readdir(fn)
     for f in files
         f1 = replace(f,".cif"=>".improved.cif")
@@ -15,7 +15,7 @@ if !isfile(fn) && ispath(fn)
         new_fn = rstrip(fn,"/")*"/$f1"
         write_to_file(join(improved,"\n"), new_fn)
     end
-elseif isfile(fn) && !ispath(fn)
+elseif isfile(fn) && !isdir(fn)
     fn1 = replace(fn,".cif"=>".improved.cif")
     improved = improve_cif("",fn)
     println(length(improved))
