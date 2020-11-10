7ce908dc20e10b2d7ff063f5a0584ffa1bcd5bb1
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-10 11:32:16 +0800

    master

--- scripts/example.jl
+++ scripts/example.jl
@@ -1,25 +1,16 @@
 using Isosuite
 ENV["ISOSUITE_FOLDER"] = "/home/dabajabaza/abinitio/iso/" ##!! change
-@inline decif(x) = first(SPLTD(x))
 
 fn = ARGS[1]
-println(ispath(fn))
-println(isfile(fn))
-
 if !isfile(fn) && isdir(fn)
     files = readdir(fn)
     for f in files
         f1 = replace(f,".cif"=>".improved.cif")
-        old_fn = rstrip(fn,"/")*"/$f"
-        improved = improve_cif("",old_fn)
-        new_fn = rstrip(fn,"/")*"/$f1"
-        write_to_file(join(improved,"\n"), new_fn)
+        improve_cif("",rstrip(fn,"/")*"/$f") ⇶ (rstrip(fn,"/")*"/$f1")
     end
 elseif isfile(fn) && !isdir(fn)
     fn1 = replace(fn,".cif"=>".improved.cif")
-    improved = improve_cif("",fn)
-    println(length(improved))
-    write_to_file(join(improved,"\n"), fn1)
+    improve_cif("",fn) ⇶ fn1
 end
 
 exit()
--- scripts/improve_cif.jl
+++ scripts/improve_cif.jl
@@ -6,13 +6,13 @@ ENV["ISOSUITE_FOLDER"] = "/home/dabajabaza/abinitio/iso/"
 @inline decif(x) = first(SPLTD(x))
 
 fn = ARGS[1]
-if !isfile(fn) && ispath(fn)
+if !isfile(fn) && isdir(fn)
     files = readdir(fn)
     for f in files
         f1 = replace(f,".cif"=>".improved.cif")
         improve_cif("",rstrip(fn,"/")*"/$f") ⇶ (rstrip(fn,"/")*"/$f1")
     end
-elseif isfile(fn) && !ispath(fn)
+elseif isfile(fn) && !isdir(fn)
     fn1 = replace(fn,".cif"=>".improved.cif")
     improve_cif("",fn) ⇶ fn1
 end
