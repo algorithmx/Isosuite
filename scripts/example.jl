2c7f47c03cc5fb0f94b1c69f7fec28095639d3eb
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-10 11:27:54 +0800

    master

---
+++ scripts/example.jl
@@ -0,0 +1,23 @@
+using Isosuite
+ENV["ISOSUITE_FOLDER"] = "/home/dabajabaza/abinitio/iso/" ##!! change
+@inline decif(x) = first(SPLTD(x))
+
+fn = ARGS[1]
+println(fn)
+if !isfile(fn) && ispath(fn)
+    files = readdir(fn)
+    for f in files
+        f1 = replace(f,".cif"=>".improved.cif")
+        old_fn = rstrip(fn,"/")*"/$f"
+        improved = improve_cif("",old_fn)
+        new_fn = rstrip(fn,"/")*"/$f1"
+        write_to_file(join(improved,"\n"), new_fn)
+    end
+elseif isfile(fn) && !ispath(fn)
+    fn1 = replace(fn,".cif"=>".improved.cif")
+    improved = improve_cif("",fn)
+    println(length(improved))
+    write_to_file(join(improved,"\n"), fn1)
+end
+
+exit()
