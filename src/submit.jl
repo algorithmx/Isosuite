9216de08f05754f977bed511b0825d83543f921b
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-19 09:43:39 +0800

    comsubs submit method : pipeline

--- src/submit.jl
+++ src/submit.jl
@@ -61,13 +61,44 @@ function run_program_with_tmp_in(
     delims = ['\n',]
     ) where {S<:AbstractString}
 
-    tmp_f = "/tmp/tmp_isosuite_$(rand(1000:2000)).in"
+    tmp_f = "/tmp/tmp_isosuite_$(rand(10000:90000)).in"
 
     write_to_file(trim_comments_pound(scripts), tmp_f)
 
     run_program_with_file(tmp_f, program; delims=delims)
 
 end
+
+
+function run_program_with_pipeline(
+    fn::AbstractString, 
+    program::Cmd
+    )
+    @assert isfile(fn)
+    res = readlines(pipeline(`cat $fn`,program))
+    if length(res) <=3 || any([occursin("Error",r) for r in res]) || any([occursin("error",r) for r in res])
+        println.(res)
+        @warn "run_program_with_file() error."
+        return String[]
+    else
+        return res
+    end 
+end
+
+
+function run_program_with_pipeline_tmp_in(
+    scripts::Vector{S}, 
+    program::Cmd
+    ) where {S<:AbstractString}
+    
+    tmp_f = "/tmp/tmp_isosuite_$(rand(10000:90000)).in"
+
+    write_to_file(trim_comments_pound(scripts), tmp_f)
+
+    run_program_with_pipeline(tmp_f, program)
+
+end
+
 
 
 ## -----------------------------------------------
@@ -102,9 +133,9 @@ function findsym_cifinput(fn_cif::AbstractString)
     return res
 end
 
-function comsubs(fn_cif::AbstractString)
+function comsubs(scripts::Vector{S}) where {S<:AbstractString}
     ENV["ISODATA"] = adslsh(ENV["ISOSUITE_FOLDER"])
-    res = run_program_with_file(fn_cif, `$(adslsh(ENV["ISOSUITE_FOLDER"]))./comsubs`)
+    res = run_program_with_pipeline_tmp_in(scripts, `$(adslsh(ENV["ISOSUITE_FOLDER"]))./comsubs`)
     return res
 end
 
