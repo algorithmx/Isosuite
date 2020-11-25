4eb7fe42fe7eb327c719be91d0136175f010d466
Yunlong Lian <algorithm.lian@gmail.com>
2020-11-26 06:28:20 +0800

    interpolate_cif

--- src/cif_op.jl
+++ src/cif_op.jl
@@ -550,7 +550,7 @@ function interpolate_cif(cif1, cif2)
     end
     sec_avg_3 = map(x->avg456(x[1],x[2]), zip(sec1[3],sec2[3]))
     return  (    minimal_cif_part1(title1*"__to__"*title2,latt_params) 
-            * ⦿(cif_with_symmetry_ops_part2(symm_ops))
+            * ⦿(sec1[2])
             * ⦿(sec_avg_3)   )
 end
 
